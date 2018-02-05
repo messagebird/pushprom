842b33126b253cbb1eeaef640e6f366e503bd81b
marcel corso <marcel@messagebird.com>
2018-02-05 13:41:07 +0100

    make-main-gogetable

--- udp.go
+++ udp.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"bytes"
-	"log"
 	"net"
+
+	"github.com/prometheus/common/log"
 )
 
 func listenUDP() {
-	log.Println("listening for stats UDP on port " + *udpListenAddress)
+	log.Info("listening for stats UDP on port " + *udpListenAddress)
 	serverAddr, err := net.ResolveUDPAddr("udp", *udpListenAddress)
 	if err != nil {
-		log.Println("Error: ", err)
+		log.Error(err)
 	}
 
 	serverConn, err := net.ListenUDP("udp", serverAddr)
 	if err != nil {
-		log.Println("Error: ", err)
+		log.Error(err)
 	}
 	defer serverConn.Close()
 
@@ -24,22 +25,22 @@ func listenUDP() {
 	for {
 		n, _, err := serverConn.ReadFromUDP(buf)
 		if err != nil {
-			log.Println("Error reading from UDP: ", err)
+			log.Error("Error reading from UDP: ", err)
 			continue
 		}
 		udpPacketCount.Inc()
 		if *debug {
-			log.Printf("new udp package: %s", string(buf[0:n]))
+			log.Debugf("new udp package: %s", string(buf[0:n]))
 		}
 		delta, err := NewDelta(bytes.NewBuffer(buf[0:n]))
 		if err != nil {
-			log.Println("Error creating delta: ", err)
+			log.Error("Error creating delta: ", err)
 			continue
 		}
 
 		err = delta.Apply()
 		if err != nil {
-			log.Println("Error applying delta: ", err)
+			log.Error("Error applying delta: ", err)
 		}
 	}
 }
