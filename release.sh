569fb73ebdd09c716b6b6bb3e1b44bf25becd355
marcel corso <marcel@messagebird.com>
2018-11-26 14:09:59 +0100

    a better release script copied form messagebird/sachet thanks @epels

--- release.sh
+++ release.sh
@@ -3,9 +3,33 @@
 # use like this: 
 #       VERSION=1.0.1 ./release.sh
 
-mkdir -p bin
-rm -rf "bin/pushprom-$VERSION.linux-amd64"
-mkdir "bin/pushprom-$VERSION.linux-amd64"
-env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o "bin/pushprom-$VERSION.linux-amd64/pushprom" github.com/messagebird/pushprom
+set -e 
+
+# github user and repo
+USER=messagebird
+REPO=pushprom
+
+rm -rf "bin/$REPO-$VERSION.linux-amd64"
+mkdir -p "bin/$REPO-$VERSION.linux-amd64"
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o "bin/sachet-$VERSION.linux-amd64/$REPO" github.com/$USER/$REPO
 cd bin
-tar -zcvf "pushprom-$VERSION.linux-amd64.tar.gz" "pushprom-$VERSION.linux-amd64"
+tar -zcvf "$REPO-$VERSION.linux-amd64.tar.gz" "$REPO-$VERSION.linux-amd64"
+
+# go get -u github.com/aktau/github-release
+# dont forget to set your token like
+# export GITHUB_TOKEN=blabla
+git tag -a $VERSION -m "version $VERSION"
+
+github-release release \
+    --user $USER \
+    --repo $REPO \
+    --tag $VERSION \
+    --name $VERSION \
+    --description "version $VERSION!" 
+
+github-release upload \
+    --user $USER \
+    --repo $REPO \
+    --tag $VERSION \
+    --name "$REPO-$VERSION.linux-amd64.tar.gz" \
+    --file "$REPO-$VERSION.linux-amd64.tar.gz"
